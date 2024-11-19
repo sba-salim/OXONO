e53de58f643aba23fd720c1b5cc5966e7c3e82b1
60283 <60283@etu.he2b.be>
2024-11-19 19:45:54 +0100

    Secure push to pull in my workstation

---
+++ projet/oxono-g60283/src/main/java/model/Board.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Board {
+    private Token[][] grid;
+    private Position posTotemX;
+    private Position posTotemO;
+    private Totem lastMoved;
+    private int size;
+    public Board() {
+
+    }
+}
